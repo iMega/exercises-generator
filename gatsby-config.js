4f554d07676ed737a502273dd190b26e1153d142
Dmitry Stoletov <irvis@imega.ru>
2021-10-15 15:30:44 +0300

    add cname
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: "https://exercises-generator.imega.ru",
+        siteUrl: "https://exercises-generator.imega.club",
         title: "Exercises generator for English",
     },
     plugins: [],
